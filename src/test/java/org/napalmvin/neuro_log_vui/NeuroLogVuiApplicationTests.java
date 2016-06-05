3470c18692ab654345e46217d7beb4970111992f
Oleg Sukhonosov <napalmvin@gmail.com>
2016-06-05 15:00:20 +0300

    Changing app root context and applied wworking security

---
+++ src/test/java/org/napalmvin/neuro_log_vui/NeuroLogVuiApplicationTests.java
@@ -0,0 +1,19 @@
+package org.napalmvin.neuro_log_vui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(
+        classes = Application.class)
+public class NeuroLogVuiApplicationTests {
+//FAIled WTF
+
+    @Test
+    public void contextLoads() {
+    }
+}
